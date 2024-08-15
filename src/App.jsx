ccdd4705361498112332bf25c3a6aa4466f8032d
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-15 11:29:50 +0330

    edit routing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+//? import rout pages
+import Layout from "./pages/Layout.jsx";
+import Products from "./pages/Products.jsx";
+
+//? react router dom
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to={`/products`} />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//? import components
 import App from "./App.jsx";
-import Layout from "./router/Layout.jsx";
-//? styles
 import "./index.css";
-
-//? react router dom
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <App />,
-      },
-    ],
-  },
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
