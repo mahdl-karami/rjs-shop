05d0379b6163ce22f2988cd82def46ed6ac20fc3
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-16 16:04:47 +0330

    add empty cart alert

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,16 +2,31 @@
 import ProductCard from "../components/ProductCard";
 //? import helpers
 import findIndex from "../helpers/findIndex";
-
+//? import icons
+import { FiShoppingCart } from "react-icons/fi";
+import { FaSlash } from "react-icons/fa";
 function Cart({ state: { allProducts, cartProducts }, dispatch }) {
   return (
-    <div className="products">
-      {cartProducts?.map((product, key) => (
-        <div key={key} className="product">
-          <ProductCard product={product} allProducts={allProducts} index={findIndex(allProducts, product)} dispatch={dispatch} />
+    <>
+      {cartProducts && cartProducts.length ? (
+        <div className="products">
+          {cartProducts?.map((product, key) => (
+            <div key={key} className="product">
+              <ProductCard product={product} allProducts={allProducts} index={findIndex(allProducts, product)} dispatch={dispatch} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className="cart-empty">
+          <FiShoppingCart />
+          <FaSlash />
+          <div>
+            <p>Cart is empty</p>
+            <p>- add some items to cart</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
